0d965bc76d3d99b242ddb2f60378bf1ea5f04180
Михаил Капелько <kornerr@gmail.com>
2026-01-15 22:07:58 +0300

    copy by cursor

--- ver-android/app/src/main/kotlin/org/opengamestudio/entities.kt
+++ ver-android/app/src/main/kotlin/org/opengamestudio/entities.kt
@@ -3,7 +3,7 @@ package org.opengamestudio
 
 
 data class BudgetContext(
-    var copyResult: Boolean = false,
+    var didClickCopy: Boolean = false,
     var didClickPaste: Boolean = false,
     var didLaunch: Boolean = false,
     var inputDate: String = "",
@@ -18,8 +18,8 @@ data class BudgetContext(
     override var recentField: String = "",
 ): CLDContext {
     override fun <T> field(name: String): T {
-        if (name == "copyResult") {
-            return copyResult as T
+        if (name == "didClickCopy") {
+            return didClickCopy as T
         } else if (name == "didClickPaste") {
             return didClickPaste as T
         } else if (name == "didLaunch") {
@@ -54,8 +54,8 @@ data class BudgetContext(
         name: String,
         value: Any?
     ) {
-        if (name == "copyResult") {
-            copyResult = value as Boolean
+        if (name == "didClickCopy") {
+            didClickCopy = value as Boolean
         } else if (name == "didClickPaste") {
             didClickPaste = value as Boolean
         } else if (name == "didLaunch") {
