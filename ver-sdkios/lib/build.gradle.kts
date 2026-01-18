1b4059ebd17a1a4bf0bffc5d09998af7e6587b7d
Михаил Капелько <kornerr@gmail.com>
2026-01-18 06:20:35 +0300

    sdk ios build

---
+++ ver-sdkios/lib/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    //alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+        }
+    }
+}
+
+kotlin {
+    val xcf = XCFramework()
+    arrayOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+        iosX64(),
+    ).forEach {
+        it.binaries.framework {
+            baseName = "sdkios"
+            xcf.add(this)
+            isStatic = false
+        }
+    }
+}
