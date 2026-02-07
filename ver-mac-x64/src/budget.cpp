f006ca6132f62a8aab3f22c4dfaee86474a0e327
Михаил Капелько <kornerr@gmail.com>
2026-02-07 16:08:48 +0300

    no callback yet

--- ver-mac-x64/src/budget.cpp
+++ ver-mac-x64/src/budget.cpp
@@ -4,10 +4,22 @@
 
 #define KT_ANY = libgb_kref_kotlin_Any
 
+void onDidLaunch(const libgb_kref_org_opengamestudio_CLDContext &c) {
+    printf("ИГР onDL got did launch\n");
+}
+
 BudgetComponent::BudgetComponent() {
     auto item = KT.Item.Item(9, 8, 7, 6);
     printf("ИГР x = '%d'\n", KT.Item.get_x(item));
     auto ctrl = KT.budgetController();
+
+    KT.CLDController.registerFieldCallback(
+        ctrl,
+        "didLaunch",
+        onDidLaunch
+        //[]() { printf("ИГР BudgetC.ctrl got did launch\n"); }
+    );
+
     
     KT.CLDController.set(ctrl, "didLaunch", KT.boolAsAny(true));
 }
