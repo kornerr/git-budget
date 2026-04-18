11b7ef8e115c3fb6c4d673172d5fb3259fb3211a
Михаил Капелько <kornerr@gmail.com>
2026-04-18 22:39:59 +0300

    budget ui

--- ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
+++ ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
@@ -43,72 +43,11 @@ class MainActivity: ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Budget(
-                        vm = VM,
-                        modifier = Modifier.padding(innerPadding),
+                        Modifier.padding(innerPadding),
+                        VM
                     )
                 }
             }
-        }
-    }
-}
-
-//<!-- Budget -->
-
-@Composable
-fun Budget(
-    modifier: Modifier = Modifier,
-    vm: VM,
-) {
-    Column(
-        modifier = modifier.fillMaxSize().padding(horizontal = 24.dp),
-        //horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row {
-            TextField(
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                modifier = Modifier.weight(2f),
-                onValueChange = {
-                    vm.inputSpent.value = it 
-                    budgetCtrl().set("inputSpent", it)
-                },
-                placeholder = { Text(vm.inputSpentPlaceholder.value) },
-                value = vm.inputSpent.value,
-            )
-            ElevatedButton(
-                modifier = Modifier.weight(1f),
-                onClick = { budgetCtrl().set("didClickPaste", true) },
-            ) {
-                Text(vm.inputSpentPasteTitle.value)
-            }
-        }
-        Row {
-            TextField(
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                modifier = Modifier.padding(top = 12.dp).weight(2f),
-                onValueChange = {
-                    vm.inputMorningBalance.value = it 
-                    budgetCtrl().set("inputMorningBalance", it)
-                },
-                placeholder = { Text(vm.inputMorningBalancePlaceholder.value) },
-                value = vm.inputMorningBalance.value,
-            )
-            ElevatedButton(
-                modifier = Modifier.weight(1f),
-                onClick = { budgetCtrl().set("didClickPasteMorningBalance", true) },
-            ) {
-                Text(vm.inputMorningBalancePasteTitle.value)
-            }
-        }
-        Text(
-            modifier = Modifier.padding(top = 12.dp),
-            style = MaterialTheme.typography.bodyMedium,
-            text = vm.result.value,
-        )
-        ElevatedButton(
-            modifier = Modifier.padding(top = 8.dp),
-            onClick = { budgetCtrl().set("didClickCopy", true) },
-        ) {
-            Text(vm.resultCopyTitle.value)
         }
     }
 }
---
+++ ver-android/app/src/main/kotlin/org/opengamestudio/budgetUI.kt
@@ -0,0 +1,93 @@
+package org.opengamestudio
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import org.opengamestudio.ui.theme.MyApplicationTheme
+
+@Composable
+fun Budget(
+    modifier: Modifier = Modifier,
+    vm: VM,
+) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(horizontal = 24.dp)
+    ) {
+        Row {
+            TextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                modifier = Modifier.weight(2f),
+                onValueChange = {
+                    vm.inputSpent.value = it 
+                    budgetCtrl().set("inputSpent", it)
+                },
+                placeholder = { Text(vm.inputSpentPlaceholder.value) },
+                value = vm.inputSpent.value,
+            )
+            ElevatedButton(
+                modifier = Modifier.weight(1f),
+                onClick = { budgetCtrl().set("didClickPaste", true) },
+            ) {
+                Text(vm.inputSpentPasteTitle.value)
+            }
+        }
+        Row {
+            TextField(
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                modifier = Modifier.padding(top = 12.dp).weight(2f),
+                onValueChange = {
+                    vm.inputMorningBalance.value = it 
+                    budgetCtrl().set("inputMorningBalance", it)
+                },
+                placeholder = { Text(vm.inputMorningBalancePlaceholder.value) },
+                value = vm.inputMorningBalance.value,
+            )
+            ElevatedButton(
+                modifier = Modifier.weight(1f),
+                onClick = { budgetCtrl().set("didClickPasteMorningBalance", true) },
+            ) {
+                Text(vm.inputMorningBalancePasteTitle.value)
+            }
+        }
+        Text(
+            modifier = Modifier.padding(top = 12.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            text = vm.result.value,
+        )
+        ElevatedButton(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = { budgetCtrl().set("didClickCopy", true) },
+        ) {
+            Text(vm.resultCopyTitle.value)
+        }
+    }
+}
