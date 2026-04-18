7fa3562c7c9a69eb99447e76d877b9d4c5f39118
Михаил Капелько <kornerr@gmail.com>
2026-04-18 22:42:10 +0300

    less imports

--- ver-android/app/src/main/kotlin/org/opengamestudio/budgetUI.kt
+++ ver-android/app/src/main/kotlin/org/opengamestudio/budgetUI.kt
@@ -1,14 +1,8 @@
 package org.opengamestudio
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
