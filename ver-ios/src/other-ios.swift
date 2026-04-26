cdc0a26a84a93a9fa6c871c3b6e610a4ae2d7f6a
Михаил Капелько <kornerr@gmail.com>
2026-04-26 22:32:53 +0300

    re

--- ver-ios/src/other-ios.swift
+++ ver-ios/src/other-ios.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-// Convert Apple weekday (1 = Sunday, 2 = Monday, …) to Monday-based (1 = Monday, …, 7 = Sunday)
+// Convert Apple weekday (1 = Sunday, 2 = Monday, …)
+// to what Java uses (1 = Monday, …, 7 = Sunday)
 func mondayBasedWeekday(_ dayIn: Int) -> Int {
     var dayOut = dayIn - 1
     if dayOut == 0 {
