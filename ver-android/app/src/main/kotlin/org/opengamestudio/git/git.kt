f9aad3782b99e3301ce506b076cdd078ab8e7196
Михаил Капелько <kornerr@gmail.com>
2026-05-02 23:04:14 +0300

    clone off

--- ver-android/app/src/main/kotlin/org/opengamestudio/git/git.kt
+++ ver-android/app/src/main/kotlin/org/opengamestudio/git/git.kt
@@ -1,10 +1,25 @@
 package org.opengamestudio
 
+import java.io.File
+import org.eclipse.jgit.api.Git
+
 //private typealias GC = GitContext
 
 object GitComponent {
     init {
-        println("ИГР GitC init")
+        println("ИГР GitC.init-01")
+
+        try {
+            val git = Git
+                .cloneRepository()
+                .setURI("https://github.com/OGStudio/kotlin-dialect")
+                .setDirectory(File("cloned-repo"))
+                .call()
+            println("ИГР GitC.init-02")
+        } catch (e: Exception) {
+            println("ИГР GitC.init-03 exception: '$e'")
+            e.printStackTrace()
+        }
     }
 
     fun setup() {
