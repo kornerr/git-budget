8491d341ab4cd9c66ccd4f0f224def6e3e3395df
Михаил Капелько <kornerr@gmail.com>
2026-05-02 22:44:46 +0300

    create git cmp

--- ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
+++ ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
@@ -13,8 +13,11 @@ class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         VM.androidContext = this
         BudgetComponent.setup()
+        GitComponent.setup()
+
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
---
+++ ver-android/app/src/main/kotlin/org/opengamestudio/git/git.kt
@@ -0,0 +1,12 @@
+package org.opengamestudio
+
+//private typealias GC = GitContext
+
+object GitComponent {
+    init {
+        println("ИГР GitC init")
+    }
+
+    fun setup() {
+    }
+}
