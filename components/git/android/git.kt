5bc7505bc497cdeb7eab500fb8578f556a6e77b0
Михаил Капелько <kornerr@gmail.com>
2026-05-10 15:50:00 +0300

    disable commit/push effects

--- components/git/android/git.kt
+++ components/git/android/git.kt
@@ -7,10 +7,10 @@ object GitComponent {
         val vm = VM
         val oneliners = arrayOf(
             F.clone, { c: GC -> gitClone(c.repoDir, c.repoURL) },
-            F.commit, { c: GC -> gitCommit(c.repoDir) },
+            //F.commit, { c: GC -> gitCommit(c.repoDir) },
             F.didLaunch, { c: GC -> gitLocateRootDir(vm.androidContext!!) },
             F.pull, { c: GC -> gitPull(c.repoDir) },
-            F.push, { c: GC -> gitPush(c.repoDir) },
+            //F.push, { c: GC -> gitPush(c.repoDir) },
             F.repoDir, { c: GC -> gitCheckRepoDirAvailability(c.repoDir) },
         )
         registerOneliners(gitCtrl(), oneliners)
