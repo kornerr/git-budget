5a59e306a4eca9246af411b7fa4b51709d6dfbe7
Михаил Капелько <kornerr@gmail.com>
2026-05-11 22:33:29 +0300

    loaded items

--- components/log/android/logEffect.kt
+++ components/log/android/logEffect.kt
@@ -4,16 +4,11 @@ import java.io.File
 
 fun logParse(lines: Array<String>) {
     val items = logParseItems(lines)
-    logSet(F.items, items)
+    logSet(F.loadedItems, items)
 }
 
 fun logReadFile(dir: String) {
     val f = File("$dir/$LOG_FILE")
     val lines = f.readLines().toTypedArray()
-    println("ИГР logR-01 begin")
-    for (ln in lines) {
-        println("ИГР logR-02 ln: '$ln'")
-    }
-    println("ИГР logR-03 end")
     logSet(F.fileLines, lines)
 }
