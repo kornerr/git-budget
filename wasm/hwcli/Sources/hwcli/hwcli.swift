976146afb8f02631949d65cf6fb251ec3c37af2c
Михаил Капелько <kornerr@gmail.com>
2026-05-12 19:30:15 +0300

    try html

--- wasm/hwcli/Sources/hwcli/hwcli.swift
+++ wasm/hwcli/Sources/hwcli/hwcli.swift
@@ -1,5 +1,17 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+private let _greeting: [CChar] = {
+    let str = "Hello from Swift WebAssembly!"
+    return str.utf8.map { CChar(bitPattern: $0) } + [0]
+}()
+
+@_expose(wasm, "get_message")
+func get_message() -> UnsafePointer<CChar> {
+    _greeting.withUnsafeBufferPointer { $0.baseAddress! }
+}
+
+@_expose(wasm, "add")
+func add(_ a: Int, _ b: Int) -> Int {
+    a + b
+}
 
 @main
 struct hwcli {
