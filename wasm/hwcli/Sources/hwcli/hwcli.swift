cc8fd8291f322f6cbc66ce9a29b42940fc3a6378
Михаил Капелько <kornerr@gmail.com>
2026-05-12 22:05:34 +0300

    before review hw wasm html

--- wasm/hwcli/Sources/hwcli/hwcli.swift
+++ wasm/hwcli/Sources/hwcli/hwcli.swift
@@ -1,21 +1,38 @@
-private let _greeting: [CChar] = {
-    let str = "Hello from Swift WebAssembly!"
-    return str.utf8.map { CChar(bitPattern: $0) } + [0]
-}()
+@MainActor
+enum DataContext {
+    static let strBuf: UnsafeMutablePointer<CChar> = {
+        let p = UnsafeMutablePointer<CChar>.allocate(capacity: 256)
+        p[0] = 72;  p[1] = 101; p[2] = 108; p[3] = 108; p[4] = 111
+        p[5] = 44;  p[6] = 32;  p[7] = 119; p[8] = 111; p[9] = 114
+        p[10] = 108; p[11] = 100; p[12] = 0
+        return p
+    }()
 
-@_expose(wasm, "get_message")
-func get_message() -> UnsafePointer<CChar> {
-    _greeting.withUnsafeBufferPointer { $0.baseAddress! }
-}
+    static let scratch = UnsafeMutablePointer<CChar>.allocate(capacity: 256)
+    static var intVal: Int32 = 153
 
-@_expose(wasm, "add")
-func add(_ a: Int, _ b: Int) -> Int {
-    a + b
-}
-
-@main
-struct hwcli {
-    static func main() {
-        print("Hello, world!")
+    static func setString(from ptr: UnsafePointer<CChar>) {
+        var i = 0
+        while i < 255 {
+            strBuf[i] = ptr[i]
+            if ptr[i] == 0 { break }
+            i += 1
+        }
+        strBuf[255] = 0
     }
 }
+
+@MainActor @_expose(wasm, "get_string")
+func get_string() -> UnsafePointer<CChar> { UnsafePointer(DataContext.strBuf) }
+
+@MainActor @_expose(wasm, "get_int")
+func get_int() -> Int32 { DataContext.intVal }
+
+@MainActor @_expose(wasm, "set_int")
+func set_int(_ value: Int32) { DataContext.intVal = value }
+
+@MainActor @_expose(wasm, "set_string")
+func set_string(_ ptr: UnsafePointer<CChar>) { DataContext.setString(from: ptr) }
+
+@MainActor @_expose(wasm, "get_scratch")
+func get_scratch() -> UnsafeMutablePointer<CChar> { DataContext.scratch }
