1132d5511611f9db10a5a0ab71f9c6e88755ad92
Михаил Капелько <kornerr@gmail.com>
2026-05-13 16:34:32 +0300

    sendany-1

---
+++ wasm/hwcli/Sources/hwcli/wcli.swift
@@ -0,0 +1,35 @@
+func sendAny(key: String, value: Any) {
+    currentDataContext.recentField = key
+    switch key {
+    case "didLaunch":
+        if let v = value as? Bool { currentDataContext.didLaunch = v }
+    case "selectedId":
+        if let v = value as? Int32 { currentDataContext.selectedId = v }
+    case "url":
+        if let v = value as? String { currentDataContext.url = v }
+    default:
+        break
+    }
+}
+
+@_expose(wasm, "sendAnyString")
+@MainActor
+func sendAnyString(_ keyPtr: UnsafePointer<CChar>, _ valuePtr: UnsafePointer<CChar>) {
+    let key = String(cString: keyPtr)
+    let value = String(cString: valuePtr)
+    sendAny(key: key, value: value)
+}
+
+@_expose(wasm, "sendAnyInt")
+@MainActor
+func sendAnyInt(_ keyPtr: UnsafePointer<CChar>, _ value: Int32) {
+    let key = String(cString: keyPtr)
+    sendAny(key: key, value: value)
+}
+
+@_expose(wasm, "sendAnyBool")
+@MainActor
+func sendAnyBool(_ keyPtr: UnsafePointer<CChar>, _ value: Bool) {
+    let key = String(cString: keyPtr)
+    sendAny(key: key, value: value)
+}
