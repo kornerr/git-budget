8b57f7dd7aabe5401cfaf46a81ba132982bb392a
Михаил Капелько <kornerr@gmail.com>
2026-05-20 21:59:35 +0300

    rm dbg

--- components/hist/sdk/histFun.kt
+++ components/hist/sdk/histFun.kt
@@ -34,7 +34,6 @@ fun histItemsToString(items: Array<HistItem>): String {
         o += "\n$HIST_PREFIX_LEFT${item.left}"
         o += "\n"
     }
-    /**/println("ИГР histITS o: '$o'")
     return o
 }
 
@@ -42,24 +41,20 @@ fun histParseItems(lines: Array<String>): Array<HistItem> {
     var items = arrayOf<HistItem>()
     var item: HistItem? = null
     for (ln in lines) {
-        /**/println("ИГР histPI-01 ln: '$ln'")
         if (ln.startsWith(HIST_PREFIX_DATE)) {
             val prefixLen = HIST_PREFIX_DATE.length
             val value = ln.substring(prefixLen)
-            /**/println("ИГР histPI-02 prefixL/value: '$prefixLen'/'$value'")
             item = HistItem()
             item.dt = value
         }
         else if (ln.startsWith(HIST_PREFIX_SPENT)) {
             val prefixLen = HIST_PREFIX_SPENT.length
             val value = ln.substring(prefixLen)
-            /**/println("ИГР histPI-03 prefixL/value: '$prefixLen'/'$value'")
             item!!.spent = value.toFloat()
         }
         else if (ln.startsWith(HIST_PREFIX_LEFT)) {
             val prefixLen = HIST_PREFIX_LEFT.length
             val value = ln.substring(prefixLen)
-            /**/println("ИГР histPI-04 prefixL/value: '$prefixLen'/'$value'")
             item!!.left = value.toFloat()
         }
 
